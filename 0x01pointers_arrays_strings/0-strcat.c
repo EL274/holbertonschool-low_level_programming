4a4750c88c10a0dd72c3d1548cbfca6102ad2d4e
EL274 <tresormakosso510@gmail.com>
2024-06-30 22:14:10 +0000

    strc

--- 0x01pointers_arrays_strings/0-strcat.c
+++ 0x01pointers_arrays_strings/0-strcat.c
@@ -11,14 +11,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i ;
+	int j ;
+	 i =0;
+	 while (dest [i]!='\0')
+	 {
+		 i++;
+	 }
+	 j=0;
+	 while (src[j]!='\0')
+	 {
+		 dest[j]=src[j];
+		 i++;
+			 j++;
+	 }
+	 dest [j]=0 '\0';
+	 return (dest);
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-	for (j = 0; src[j] != '\0'; j++)
-	{
-		dest[i] = src[j];
-		i++;
-	}
-	return (dest);
 }
