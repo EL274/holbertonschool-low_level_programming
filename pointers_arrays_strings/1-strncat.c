862c02171204bdbe63f0bea3804074813ce7fca8
EL274 <tresormakosso510@gmail.com>
2024-07-01 11:35:38 +0000

    hot

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+# include "main.h"
+char*strncat(char*dest,char*src,int n)
+{ int i 
+	int j
+		i=0
+		while (dest[i] !='\0')
+
+		{
+			i++;
+		}
+               j=0
+	       while ( src[j]!='\0' && j < n );
+	       {
+	        dest [i]=src[j];
+                  i++;
+	          j++;
+
+
+	       }
+          
+              dest[j] !='\0';
+              return(dest);	      
