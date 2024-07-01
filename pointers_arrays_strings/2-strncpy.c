db2dfd3d5e1f722b48c6ccc3d8d52bd14cc450a1
EL274 <tresormakosso510@gmail.com>
2024-07-01 13:22:23 +0000

    let's play

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,19 +8,23 @@
  *
  * Return: pointer to the resulting string
  */
-char *_strncpy(char *dest, char *src, int n);
-int i 
- i=0
- while (src [i] !='\0' && i<0);
+char *_strncpy(char *dest, char *src, int n)
 {
-	dest[i]=src[i];
-	i++;
-}
-while (i<0);
-     {  
-	dest[i]!='\0';
-	i++;
-     } 
+	int i;
 
-	return(dest); 
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
 }
