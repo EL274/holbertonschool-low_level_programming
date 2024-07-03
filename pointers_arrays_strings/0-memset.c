4253da5710476639357cd9f179021b12dd88ff62
EL274 <tresormakosso510@gmail.com>
2024-07-03 10:54:20 +0000

    set

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+/**# include "main.h"
+*
+* _memset -Entry point 
+* @s destination pointed
+* @ b constant byte b
+* @ n bytes 
+*return always 0 (success)
+*/
+ char *_memset(char *s, char b, unsigned int n);
+{ 
+	usigned int i;
+	for (i=0 ; i<n ; i++){
+		s[i]=b;
+	}
+	return s;
+}
