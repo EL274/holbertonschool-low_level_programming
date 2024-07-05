896aa606a9c57cefe46bebed9dd989cf0d34714f
EL274 <tresormakosso510@gmail.com>
2024-07-05 15:15:58 +0000

    the moment

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the number of arguments passed into it
+ *
+ * @args: This is the argument count
+ * @argv: This is the argument vector
+ *
+ * Return: This return to 0
+ *
+ */
+int main ( int arg s ; char * arg v[])
+{
+	printf("%d/n")
+	(void)argv;
+	return(0);
+}
