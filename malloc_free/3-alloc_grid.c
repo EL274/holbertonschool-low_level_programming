ce9c629f8d3168c57cd4aefa7c9fbe98ccb6ba01
EL274 <tresormakosso510@gmail.com>
2024-07-10 13:48:43 +0000

    finally

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -7,35 +7,30 @@
  *
  *
  *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
  */
-unsigned int _strspn(char *s, char *accept)
-{
-	int i, j;
-	int c = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] != 32)
-		{
-			for (j = 0; accept[j] != '\0'; j++)
-			{
-				if (s[i] == accept[j])
-					c++;
-			}
-		}
-		else
-			return (c);
-
+int **alloc_grid(int width, int height)
+{       int i,j;
+	int** array=(int**)malloc(width *sizeof(int*));
+	if (array == NULL){
+return NULL;
 	}
-	return (c);
+	for( i=0; i < width; i++){
+		array[i]=(int*)malloc(height *sizeof(int));
+		if (array[i] == NULL)
+		{
+			for (j=0; j<i ;j++){
+				free(array[j]);
+			}
+			free(array);
+			return NULL;
+		}
+	}
+	for(i=0 ; i < width ; i++){
+		for(j=0 ;j <height ;j++){
+			array[i]=0;
+		}
+	}
+	return array;
 }
+
+
