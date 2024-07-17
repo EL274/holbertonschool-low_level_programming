20f850d13e35be1d00743119589061e16c0cbc4d
EL274 <tresormakosso510@gmail.com>
2024-07-17 09:44:39 +0000

    don't give up

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,14 +1,17 @@
 #include "function_pointers.h"
 #include <stdio.h>
-#include <stdlib.h>
-
-void print_name(char *name, void (*f)(char *))
+/**
+ *array_iterator:function given as a parameter on each element of an array
+ *@size: size of the array
+ *@action: pointer to function
+ *Return:void
+ *
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array != NULL && action != NULL && size >0)
-	{
-		for(i = 0; i < size ; i++)
-		{
-		action(array[i]);
-		}
-	}
+	size_t i;
+
+	if (array && size && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
