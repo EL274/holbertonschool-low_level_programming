f0e1995b88d3532f84b31100c0edac0fbb29b8bc
EL274 <tresormakosso510@gmail.com>
2024-08-02 11:26:03 +0000

    list all of linked

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include"lists.h"
+#include<stdio.h>
+/**
+ *sum_dlistint - sum of all the data (n) of a dlistint_t linked list.
+ *@head: pointer to the head of doubly linked lists
+ *
+ *Return: returns the sum of all the data (n) of a dlistint_t linked list
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	unsigned int sum = 0;
+
+	while(head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (head);
+}
