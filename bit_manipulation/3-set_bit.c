8cb848949737f7f1dfd037ae0602a061ed77ae71
EL274 <tresormakosso510@gmail.com>
2024-08-04 11:24:37 +0000

    clear bit

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,7 +1,7 @@
 #include"main.h"
-#include<stdio.h>
+#include <stdio.h>
 /**
- *set_bit - sets the value of a bit to 1 at a given index.
+ *set_bit - Sets the value of a bit to 1 at a given index.
  *@n: Pointer to the list
  *@index: the index, starting from 0 of the bit you want to se
  *
@@ -12,7 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >=(sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n |= (1UL << index);
+	*n ^= (1UL << index);
 
 	return (1);
 }
