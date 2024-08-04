bacd10aff24305062feff1f2c55583e5489e7389
EL274 <tresormakosso510@gmail.com>
2024-08-04 11:01:53 +0000

    set the bit

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -5,13 +5,14 @@
  *@n: Pointer to the list
  *@index: the index, starting from 0 of the bit you want to se
  *
- * Return:1 if it worked, or -1 if an error occurred
+ * Return: 1 if it worked, or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >=(sizeof(unsigned long int) * 8))
 		return (-1);
-	*n |=(1UL << index);
+
+	*n |= (1UL << index);
 
 	return (1);
 }
